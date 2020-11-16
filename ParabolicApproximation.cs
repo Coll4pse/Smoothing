171cf8468bc475ab0f735ebf69ec183423bcd2ea
Stepanov Mikhail <mihanoid10@yandex.ru>
2020-11-16 17:08:44 +0500

    Implement ParabolicApproximation.cs

--- ParabolicApproximation.cs
+++ ParabolicApproximation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Smoothing
 {
@@ -6,7 +7,25 @@ namespace Smoothing
     {
         public static Polynomial Evaluate(double[] x, double[] y)
         {
-            throw new NotImplementedException();
+            var sqrX = x.Select(x => x * x);
+            var cubeX = x.Select(x => x * x * x);
+            var quadX = x.Select(x => x * x * x * x);
+            var xy = x.Zip(y, (x, y) => x * y);
+            var sqrXy = sqrX.Zip(y, (x, y) => x * y);
+            var aCoefficients = new Matrix(new double[,]
+            {
+                {quadX.Sum(), cubeX.Sum(), sqrX.Sum()},
+                {cubeX.Sum(), sqrX.Sum(), x.Sum()},
+                {sqrX.Sum(), x.Sum(), x.Length}
+            });
+            var bCoefficients = new Matrix(new double[,]
+            {
+                {sqrXy.Sum()},
+                {xy.Sum()},
+                {y.Sum()}
+            });
+            var abc = Gauss.Evaluate(aCoefficients, bCoefficients);
+            return new Polynomial(abc[^1], abc[^2], abc[^3]);
         }
     }
 }
