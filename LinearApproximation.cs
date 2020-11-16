03e1ac478d3747f07c929e4ca199ba32765bf1f4
Stepanov Mikhail <mihanoid10@yandex.ru>
2020-11-16 22:20:41 +0500

    Implement LinearApproximation.cs

--- LinearApproximation.cs
+++ LinearApproximation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Smoothing
 {
@@ -6,7 +7,20 @@ namespace Smoothing
     {
         public static Polynomial Evaluate(double[] x, double[] y)
         {
-            throw new NotImplementedException();
+            var sqrX = x.Select(x => x * x);
+            var xy = x.Zip(y, (x, y) => x * y);
+            var aCoefficients = new Matrix(new double[,]
+            {
+                {sqrX.Sum(), x.Sum()},
+                {x.Sum(), x.Length}
+            });
+            var bCoefficients = new Matrix(new double[,]
+            {
+                {xy.Sum()},
+                {y.Sum()}
+            });
+            var abc = Gauss.Evaluate(aCoefficients, bCoefficients);
+            return new Polynomial(abc[^1], abc[^2]);
         }
     }
 }
